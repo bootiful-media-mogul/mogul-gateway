324136c9ba4e683b2060f231e868a246a892b3f4
Josh Long <54473+joshlong@users.noreply.github.com>
2025-12-24 14:16:22 -0800

    up

--- src/main/java/com/joshlong/mogul/gateway/SecurityConfiguration.java
+++ src/main/java/com/joshlong/mogul/gateway/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.joshlong.mogul.gateway;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.boot.security.autoconfigure.actuate.web.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
