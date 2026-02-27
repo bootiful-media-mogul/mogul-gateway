87dc14d4f7343259096e3c043cb25e2547d3a87c
Josh Long <54473+joshlong@users.noreply.github.com>
2026-02-27 02:59:15 -0800

    there's something wrong with the state machine. need to see wht hapopoens if i empty out a value in the config. right now, itll work.

--- src/main/java/com/joshlong/mogul/gateway/MogulSettingsAwareReactiveClientRegistrationRepository.java
+++ src/main/java/com/joshlong/mogul/gateway/MogulSettingsAwareReactiveClientRegistrationRepository.java
@@ -29,18 +29,18 @@ import java.util.Objects;
 @Component
 class CurrentToken {
 
-	private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
+    private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
 
-	CurrentToken(ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-		this.authorizedClientService = authorizedClientService;
-	}
+    CurrentToken(ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
 
-	@NonNull
-	Mono<String> getAccessToken(OAuth2AuthenticationToken authentication) {
-		return authorizedClientService //
-			.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName()) //
-			.map(client -> client.getAccessToken().getTokenValue());
-	}
+    @NonNull
+    Mono<String> getAccessToken(OAuth2AuthenticationToken authentication) {
+        return authorizedClientService //
+                .loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName()) //
+                .map(client -> client.getAccessToken().getTokenValue());
+    }
 
 }
 
@@ -52,145 +52,148 @@ class CurrentToken {
  */
 class MogulSettingsAwareReactiveClientRegistrationRepository implements ReactiveClientRegistrationRepository {
 
-	private static final String WORDPRESS_CONSTANT = "wordpress";
+    private static final String WORDPRESS_CONSTANT = "wordpress";
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-	private final ObjectProvider<CurrentToken> token;
+    private final ObjectProvider<CurrentToken> token;
 
-	private final SettingsClient settings;
+    private final SettingsClient settings;
 
-	private final ClientRegistration auth0ClientRegistration;
+    private final ClientRegistration auth0ClientRegistration;
 
-	private final boolean traceEnabled = this.log.isTraceEnabled();
+    private final boolean traceEnabled = this.log.isTraceEnabled();
 
-	private final Cache<String, ClientRegistration> clientRegistrationCache = Caffeine.newBuilder()
-		.expireAfterWrite(Duration.ofSeconds(30))
-		.maximumSize(10_000)
-		.build();
+    private final Cache<String, ClientRegistration> clientRegistrationCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofSeconds(30))
+            .maximumSize(10_000)
+            .build();
 
-	MogulSettingsAwareReactiveClientRegistrationRepository(ObjectProvider<CurrentToken> token, Environment environment,
-			SettingsClient settings) {
-		Assert.notNull(token, "token cannot be null");
-		Assert.notNull(environment, "environment cannot be null");
-		Assert.notNull(settings, "settings cannot be null");
-		this.token = token;
-		this.settings = settings;
-		this.auth0ClientRegistration = this.registerAuth0Client(environment);
-	}
+    MogulSettingsAwareReactiveClientRegistrationRepository(ObjectProvider<CurrentToken> token, Environment environment,
+                                                           SettingsClient settings) {
+        Assert.notNull(token, "token cannot be null");
+        Assert.notNull(environment, "environment cannot be null");
+        Assert.notNull(settings, "settings cannot be null");
+        this.token = token;
+        this.settings = settings;
+        this.auth0ClientRegistration = this.registerAuth0Client(environment);
+    }
 
-	@Override
-	public Mono<ClientRegistration> findByRegistrationId(String registrationId) {
+    @Override
+    public Mono<ClientRegistration> findByRegistrationId(String registrationId) {
 
-		// fast path.
-		if (registrationId.equals(auth0ClientRegistration.getRegistrationId())) {
-			return Mono.just(auth0ClientRegistration);
-		}
+        // fast path.
+        if (registrationId.equals(auth0ClientRegistration.getRegistrationId())) {
+            return Mono.just(auth0ClientRegistration);
+        }
 
-		// otherwise load settings
-		return ReactiveSecurityContextHolder //
-			.getContext() //
-			.flatMap(sc -> {
-				var auth = Objects.requireNonNull(sc.getAuthentication());
-				if (auth instanceof OAuth2AuthenticationToken auth2AuthenticationToken) {
-					var currentToken = this.token.getIfAvailable();
-					Assert.notNull(currentToken, "currentToken cannot be null");
-					return currentToken.getAccessToken(auth2AuthenticationToken)
-						.map(access -> new PrincipalTokenRegistrationId(auth2AuthenticationToken, access,
-								auth2AuthenticationToken.getAuthorizedClientRegistrationId()));
-				} //
-				return Mono.error(new IllegalStateException(String.format(
-						"no token available for authentication %s while attempting to look up secondary %s", auth,
-						ClientRegistration.class.getName())));
-			})//
-			.flatMap(this::getClientRegistration);
-	}
+        // otherwise load settings
+        return ReactiveSecurityContextHolder //
+                .getContext() //
+                .flatMap(sc -> {
+                    var auth = Objects.requireNonNull(sc.getAuthentication());
+                    if (auth instanceof OAuth2AuthenticationToken auth2AuthenticationToken) {
+                        var currentToken = this.token.getIfAvailable();
+                        Assert.notNull(currentToken, "currentToken cannot be null");
+                        return currentToken.getAccessToken(auth2AuthenticationToken)
+                                .map(access -> new PrincipalTokenRegistrationId(auth2AuthenticationToken, access,
+                                        auth2AuthenticationToken.getAuthorizedClientRegistrationId()));
+                    } //
+                    return Mono.error(new IllegalStateException(String.format(
+                            "no token available for authentication %s while attempting to look up secondary %s", auth,
+                            ClientRegistration.class.getName())));
+                })//
+                .flatMap(this::getClientRegistration);
+    }
 
-	private Mono<ClientRegistration> getClientRegistration(PrincipalTokenRegistrationId accessToken) {
-		var key = this.buildValidCacheKey(accessToken);
-		var cached = this.clientRegistrationCache.getIfPresent(key);
-		if (cached != null) {
-			if (this.traceEnabled) {
-				this.log.trace("found cached client registration for {}: {}", key, cached.getRegistrationId());
-			}
-			return Mono.just(cached);
-		}
-		if (this.traceEnabled) {
-			this.log.trace("no cached client registration for {}. loading from settings.", key);
-		}
-		return this.settings.getSettings(accessToken.accessToken())
-			.filter(sp -> sp.category().equals(WORDPRESS_CONSTANT))
-			.singleOrEmpty()
-			.flatMap(this::registerWordpressClient)
-			.doOnNext(cr -> this.clientRegistrationCache.put(key, cr));
-	}
+    private Mono<ClientRegistration> getClientRegistration(PrincipalTokenRegistrationId accessToken) {
+        var key = this.buildValidCacheKey(accessToken);
+        var cached = this.clientRegistrationCache.getIfPresent(key);
+        if (cached != null) {
+            if (this.traceEnabled) {
+                this.log.trace("found cached client registration for {}: {}", key, cached.getRegistrationId());
+            }
+            return Mono.just(cached);
+        }
+        if (this.traceEnabled) {
+            this.log.trace("no cached client registration for {}. loading from settings.", key);
+        }
+        return this.settings.getSettings(accessToken.accessToken())
+                .filter(sp -> sp.category().equals(WORDPRESS_CONSTANT))
+                .singleOrEmpty()
+                .flatMap(this::registerWordpressClient)
+                .doOnNext(cr -> this.clientRegistrationCache.put(key, cr));
+    }
 
-	private record PrincipalTokenRegistrationId(OAuth2AuthenticationToken authentication, String accessToken,
-			String registrationId) {
-	}
+    private record PrincipalTokenRegistrationId(OAuth2AuthenticationToken authentication, String accessToken,
+                                                String registrationId) {
+    }
 
-	private String buildValidCacheKey(PrincipalTokenRegistrationId token) {
-		return this.buildValidCacheKey(token.authentication(), token.registrationId());
-	}
+    private String buildValidCacheKey(PrincipalTokenRegistrationId token) {
+        return this.buildValidCacheKey(token.authentication(), token.registrationId());
+    }
 
-	private String buildValidCacheKey(Authentication authentication, String registrationId) {
-		Assert.notNull(authentication, "authentication cannot be null");
-		Assert.hasText(registrationId, "registrationId cannot be null");
-		return registrationId + "|" + authentication.getName();
-	}
+    private String buildValidCacheKey(Authentication authentication, String registrationId) {
+        Assert.notNull(authentication, "authentication cannot be null");
+        Assert.hasText(registrationId, "registrationId cannot be null");
+        return registrationId + "|" + authentication.getName();
+    }
 
-	/**
-	 * registers an OAuth client for WordPress, whose definition we can only derive from
-	 * the downstream {@code settings} table, accessed via the GraphQL settings API.
-	 */
-	private Mono<ClientRegistration> registerWordpressClient(SettingsPage settingsPage) {
-		var authorizationUri = this.getSettingsValueForKey(settingsPage, "authorizationUri");
-		var tokenUri = this.getSettingsValueForKey(settingsPage, "tokenUri");
-		var clientId = this.getSettingsValueForKey(settingsPage, "clientId");
-		var clientSecret = this.getSettingsValueForKey(settingsPage, "clientSecret");
-		var good = StringUtils.hasText(clientId) && StringUtils.hasText(clientSecret)
-				&& StringUtils.hasText(authorizationUri) && StringUtils.hasText(tokenUri);
-		if (!good) {
-			return Mono.error(new IllegalArgumentException(
-					"missing required settings for [" + WORDPRESS_CONSTANT + "] client registration"));
-		}
-		return Mono.just(ClientRegistration.withRegistrationId(WORDPRESS_CONSTANT)//
-			.clientId(clientId)//
-			.clientSecret(clientSecret)//
-			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)//
-			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE) //
-			.redirectUri("{baseUrl}/oauth2/code/{registrationId}") //
-			.scope("global") //
-			.authorizationUri(authorizationUri) //
-			.tokenUri(tokenUri) //
-			.clientName(WORDPRESS_CONSTANT) //
-			.build());
-	}
+    /**
+     * registers an OAuth client for WordPress, whose definition we can only derive from
+     * the downstream {@code settings} table, accessed via the GraphQL settings API.
+     */
+    private Mono<ClientRegistration> registerWordpressClient(SettingsPage settingsPage) {
+        var authorizationUri = this.getSettingsValueForKey(settingsPage, "authorizationUri");
+        var tokenUri = this.getSettingsValueForKey(settingsPage, "tokenUri");
+        var clientId = this.getSettingsValueForKey(settingsPage, "clientId");
+        var clientSecret = this.getSettingsValueForKey(settingsPage, "clientSecret");
+        var good = StringUtils.hasText(clientId) && StringUtils.hasText(clientSecret)
+                && StringUtils.hasText(authorizationUri) && StringUtils.hasText(tokenUri);
+        if (!good) {
+            if (this.traceEnabled) {
+                var msg = "missing required settings for [" + WORDPRESS_CONSTANT + "] client registration";
+                this.log.trace( msg);
+            }
+            return Mono.empty();
+        }
+        return Mono.just(ClientRegistration.withRegistrationId(WORDPRESS_CONSTANT)//
+                .clientId(clientId)//
+                .clientSecret(clientSecret)//
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)//
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE) //
+                .redirectUri("{baseUrl}/oauth2/code/{registrationId}") //
+                .scope("global") //
+                .authorizationUri(authorizationUri) //
+                .tokenUri(tokenUri) //
+                .clientName(WORDPRESS_CONSTANT) //
+                .build());
+    }
 
-	private ClientRegistration registerAuth0Client(Environment environment) {
-		var name = "auth0";
-		return ClientRegistrations
-			.fromOidcIssuerLocation(Objects.requireNonNull(environment.getProperty("auth0.domain")))
-			.registrationId(name)
-			.clientId(environment.getProperty("AUTH0_CLIENT_ID"))
-			.clientSecret(environment.getProperty("AUTH0_CLIENT_SECRET"))
-			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-			.redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-			.scope("profile", "openid", "email")
-			.clientName(name)
-			.build();
-	}
+    private ClientRegistration registerAuth0Client(Environment environment) {
+        var name = "auth0";
+        return ClientRegistrations
+                .fromOidcIssuerLocation(Objects.requireNonNull(environment.getProperty("auth0.domain")))
+                .registrationId(name)
+                .clientId(environment.getProperty("AUTH0_CLIENT_ID"))
+                .clientSecret(environment.getProperty("AUTH0_CLIENT_SECRET"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("profile", "openid", "email")
+                .clientName(name)
+                .build();
+    }
 
-	private String getSettingsValueForKey(SettingsPage settingsPage, String k) {
-		Assert.notNull(settingsPage, "the settingsPage must not be null");
-		Assert.notNull(k, "the key must not be null");
-		for (var setting : settingsPage.settings()) {
-			if (setting.name().equals(k)) {
-				return setting.value();
-			}
-		}
-		return null;
-	}
+    private String getSettingsValueForKey(SettingsPage settingsPage, String k) {
+        Assert.notNull(settingsPage, "the settingsPage must not be null");
+        Assert.notNull(k, "the key must not be null");
+        for (var setting : settingsPage.settings()) {
+            if (setting.name().equals(k)) {
+                return setting.value();
+            }
+        }
+        return null;
+    }
 
 }
