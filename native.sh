a09a17c72915c394dd7ce58ec3c72a4801b4e7df
Josh Long <joshua.long@broadcom.com>
2026-09-21 02:29:41 -0700

    old

--- native.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf target
-./mvnw spring-javaformat:apply
-./mvnw -DskipTests -Pnative native:compile
-DEBUG=true ./target/mogul-gateway
--- save.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-./mvnw spring-javaformat:apply
-git commit -a
-git push
